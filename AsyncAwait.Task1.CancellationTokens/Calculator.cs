dcd53aefcb7430fb8651c1c2fbfe22747de6ea62
Maxim Mochalkin <Maxim_Mochalkin@epam.com>
2023-11-11 15:59:29 +0600

    add implementation of AsyncAwait task

---
+++ AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens
+{
+    internal static class Calculator
+    {
+        public static async Task<long> Calculate(int n, CancellationToken token)
+        {
+            long sum = 0;
+            var taskFactory = new TaskFactory(token);
+            await taskFactory.StartNew(() =>
+            {
+
+                for (var i = 0; i < n; i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        token.ThrowIfCancellationRequested();
+                    }
+
+                    // i + 1 is to allow 2147483647 (Max(Int32)) 
+                    sum = sum + (i + 1);
+                }
+
+            }, token);
+
+            return sum;
+        }
+    }
+}
---
+++ AsyncAwait.Task1.CancellationTokens/Program.cs
@@ -0,0 +1,70 @@
+﻿using AsyncAwait.Task1.CancellationTokens;
+
+Console.WriteLine("Mentoring program L2. Async/await.V1. Task 1");
+Console.WriteLine("Calculating the sum of integers from 0 to N.");
+Console.WriteLine("Use 'q' key to exit...");
+Console.WriteLine();
+
+Console.WriteLine("Enter N: ");
+
+var input = Console.ReadLine();
+var cts = new CancellationTokenSource();
+while (input?.Trim().ToUpper() != "Q")
+{
+    try
+    {
+        if (int.TryParse(input, out var n) && n > 0)
+        {
+            Console.WriteLine($"The task for {n} started... Enter N to cancel the request or 'c' to continue:");
+            var newInput = Console.ReadLine();
+
+            if (int.TryParse(newInput, out var newN) && newN > 0)
+            {
+                cts.Cancel();
+                await CalculateSum(newN, cts, true).ConfigureAwait(false);
+            }
+
+            if (!cts.Token.IsCancellationRequested)
+            {
+                await CalculateSum(n, cts).ConfigureAwait(false);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Invalid integer: '{input}'. Please try again.");
+            Console.WriteLine("Enter N: ");
+        }
+
+        input = Console.ReadLine();
+    }
+    catch (OperationCanceledException e)
+    {
+        Console.WriteLine("Calculation aborted. Enter a new N to restart or press Enter to exit.");
+    }
+}
+
+Console.WriteLine("Press any key to continue");
+Console.ReadLine();
+
+static async Task CalculateSum(int n, CancellationTokenSource cts, bool isNewValue = false)
+{
+    if (!isNewValue)
+    {
+        var result = await Calculator.Calculate(n, cts.Token).ConfigureAwait(false);
+        Console.WriteLine($"Sum for {n} = {result}.");
+        Console.WriteLine();
+        Console.WriteLine("Enter N: ");
+    }
+    else
+    {
+        if (cts.Token.IsCancellationRequested)
+        {
+            var newCts = new CancellationTokenSource();
+            var result = await Calculator.Calculate(n, newCts.Token).ConfigureAwait(false);
+            Console.WriteLine($"Sum for {n} = {result}.");
+            Console.WriteLine();
+            Console.WriteLine("Enter N: ");
+        }
+    }
+    
+}
